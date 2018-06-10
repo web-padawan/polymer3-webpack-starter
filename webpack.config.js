4d65546538702f15efc0570d880eb6276e8c1e11
web-padawan <serguey.kulikov@gmail.com>
2018-06-10 19:15:20 +0300

    Use webpack-merge and .babelrc for cleaner config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,16 @@
 'use strict';
 
 const {resolve, join} = require('path');
-const webpack = require('webpack');
+const merge = require('webpack-merge');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const pkg = require('./package.json');
 
-const ENV = process.argv.find(arg => arg.includes('mode=production')) ? 'production' : 'development';
-const IS_DEV_SERVER = process.argv.find(arg => arg.includes('webpack-dev-server'));
-const OUTPUT_PATH = IS_DEV_SERVER ? resolve('src') : resolve('dist');
+const ENV = process.argv.find(arg => arg.includes('production')) ? 'production' : 'development';
+const OUTPUT_PATH = ENV === 'production' ? resolve('dist') : resolve('src');
 
-const processEnv = {
-  NODE_ENV: JSON.stringify(ENV),
-  appVersion: JSON.stringify(pkg.version)
-};
-
-/**
- * === Copy static files configuration
- */
-const copyStatics = {
-  copyWebcomponents: [{
+const polyfills = [
+  {
     from: resolve('./node_modules/@webcomponents/webcomponentsjs/webcomponents-*.js'),
     to: join(OUTPUT_PATH, 'vendor'),
     flatten: true
@@ -32,8 +22,11 @@ const copyStatics = {
     from: resolve('./node_modules/@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js'),
     to: join(OUTPUT_PATH, 'vendor'),
     flatten: true
-  }],
-  copyOthers: [{
+  }
+];
+
+const assets = [
+  {
     from: resolve('./src/vendor/babel-helpers.min.js'),
     to: join(OUTPUT_PATH, 'vendor')
   }, {
@@ -42,88 +35,75 @@ const copyStatics = {
   }, {
     from: resolve('./src/employees.json'),
     to: OUTPUT_PATH
-  }]
-};
+  }
+];
 
-/**
- * Plugin configuration
- */
-let plugins = IS_DEV_SERVER ? [] : [new CleanWebpackPlugin([OUTPUT_PATH], {verbose: true})];
-
-plugins = plugins.concat([IS_DEV_SERVER ?
-  new CopyWebpackPlugin(copyStatics.copyWebcomponents) :
-  new CopyWebpackPlugin(
-    [].concat(copyStatics.copyWebcomponents, copyStatics.copyOthers)
-  )
-]).concat([
-  new webpack.DefinePlugin({'process.env': processEnv}),
-  new HtmlWebpackPlugin({
-    template: resolve('./src/index.html')
-  })
+const commonConfig = merge([
+  {
+    entry: './src/index.js',
+    devtool: 'cheap-module-source-map',
+    output: {
+      path: OUTPUT_PATH,
+      filename: '[name].[chunkhash:8].js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: resolve('./src/index.html')
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(js|mjs)$/,
+          // We need to transpile Polymer, do not exclude node_modules
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                babelrc: true,
+                extends: join(__dirname + '/.babelrc'),
+                cacheDirectory: true
+              }
+            },
+            {
+              loader: 'uglify-template-string-loader'
+            }
+          ]
+        }
+      ]
+    }
+  },
 ]);
 
-module.exports = {
-  entry: './src/index.js',
-  devtool: 'cheap-module-source-map',
-  output: {
-    path: OUTPUT_PATH,
-    filename: '[name].[chunkhash:8].js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|mjs)$/,
-        // We need to transpile Polymer, do not exclude node_modules
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              plugins: [
-                require('@babel/plugin-external-helpers'),
-                require('@babel/plugin-syntax-dynamic-import'),
-                require('@babel/plugin-transform-classes'),
-                require('@babel/plugin-syntax-object-rest-spread'),
-                require('@babel/plugin-transform-arrow-functions'),
-                require('@babel/plugin-transform-async-to-generator'),
-                require('@babel/plugin-transform-block-scoped-functions'),
-                require('@babel/plugin-transform-block-scoping'),
-                require('@babel/plugin-transform-computed-properties'),
-                require('@babel/plugin-transform-destructuring'),
-                require('@babel/plugin-transform-duplicate-keys'),
-                require('@babel/plugin-transform-exponentiation-operator'),
-                require('@babel/plugin-transform-for-of'),
-                require('@babel/plugin-transform-function-name'),
-                require('@babel/plugin-transform-instanceof'),
-                require('@babel/plugin-transform-literals'),
-                require('@babel/plugin-transform-modules-amd'),
-                require('@babel/plugin-transform-object-super'),
-                require('@babel/plugin-transform-parameters'),
-                require('@babel/plugin-transform-regenerator'),
-                require('@babel/plugin-transform-shorthand-properties'),
-                require('@babel/plugin-transform-spread'),
-                require('@babel/plugin-transform-sticky-regex'),
-                require('@babel/plugin-transform-template-literals'),
-                require('@babel/plugin-transform-typeof-symbol'),
-                require('@babel/plugin-transform-unicode-regex')
-              ]
-            }
-          },
-          {
-            loader: 'uglify-template-string-loader'
-          }
-        ]
-      }
-    ]
-  },
-  plugins,
-  devServer: {
-    contentBase: OUTPUT_PATH,
-    compress: true,
-    overlay: {
-      errors: true
-    },
-    port: 3000,
-    host: '0.0.0.0',
-    historyApiFallback: true
+const developmentConfig = merge([
+  {
+    plugins: [
+      new CopyWebpackPlugin(polyfills)
+    ],
+    devServer: {
+      contentBase: OUTPUT_PATH,
+      compress: true,
+      overlay: true,
+      port: 3000,
+      host: '0.0.0.0',
+      historyApiFallback: true
+    }
   }
+]);
+
+const productionConfig = merge([
+  {
+    plugins: [
+      new CleanWebpackPlugin([OUTPUT_PATH], {verbose: true}),
+      new CopyWebpackPlugin([...polyfills, ...assets])
+    ]
+  }
+]);
+
+module.exports = mode => {
+  if (mode === 'production') {
+    return merge(commonConfig, productionConfig, {mode});
+  }
+
+  return merge(commonConfig, developmentConfig, {mode});
 };
