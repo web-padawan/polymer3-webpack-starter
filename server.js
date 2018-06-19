f064ed1a3976132ef16fb0449dea5ddd8e01c2cc
web-padawan <serguey.kulikov@gmail.com>
2018-06-19 18:42:06 +0300

    Update server.js to use port env variable

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback');
+const port = process.env.PORT || 8000;
 
 const app = express();
 
 app.use(history());
 app.use(serveStatic(__dirname + '/dist/'));
-app.listen(8000);
+app.listen(port);
 
-console.info('Project is running at http://localhost:8000');
+console.info(`Project is running at http://localhost:${port}`);
